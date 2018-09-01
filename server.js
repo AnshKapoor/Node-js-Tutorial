7d5796da4d90bde117f14b28b7325bf6f644790a
Ansh Kapoor <ansh605@gmail.com>
2018-09-01 23:57:09 +0530

    Another method for routing

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 var http = require('http'); 
 var path = require('path');
-var pages = [   {route: '', 
-output: 'Woohoo!'},   {route: 
-'about', output: "A simple routing with Node example"},   
-{route: 'another page', 
-output: function() {return 
-'Here\s'       
-+this.route;}}, ]; /* Array 
-for routing to different 
-pages*/
+var pages = [{id: '1', route: '', output: 'Woohoo!'},   {id: '2', route: 'about', output: 'A simple routing with Node       example'},   {id: '3', route: 'another page', output: function () {     return 'Here\s' +       this.route; }   }];
 
 http.createServer(function 
 (request, response) {   
-var lookup = 
-path.basename(decodeURI(request.url));
-pages.forEach(function(page) {     if (page.route === lookup) {       response.writeHead(200, {'Content-Type': 'text/html'});       response.end(typeof page.output === 'function'       ? page.output() : page.output);     }   });   if (!response.finished) {      response.writeHead(404);      response.end('Page Not Found!');   }
+var id = url.parse(decodeURI(request.url), true).query.id;   
+if (id) 
+{
+pages.forEach(function (page) {       if (page.id === id) {         response.writeHead(200, {'Content-Type': 'text/html'});         response.end(typeof page.output === 'function'         ? page.output() : page.output);       }     });   }   if (!response.finished) {     response.writeHead(404);     response.end('Page Not Found');   } }).listen(8080);
 
-response.writeHead(200, 
-{'Content-Type': 
-'text/html'});   
-response.end('Woohoo!'); 
-}).listen(8080);
+
