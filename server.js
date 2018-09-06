0b8b2679e4251529d0d745ae4decebe9529e76d8
Ansh Kapoor <ansh605@gmail.com>
2018-09-06 11:48:13 +0530

    Added static content

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
 var http = require('http'); 
 var path = require('path');
-var pages = [{id: '1', route: '', output: 'Woohoo!'},   {id: '2', route: 'about', output: 'A simple routing with Node       example'},   {id: '3', route: 'another page', output: function () {     return 'Here\s' +       this.route; }   }];
-
-http.createServer(function 
-(request, response) {   
-var id = url.parse(decodeURI(request.url), true).query.id;   
-if (id) 
-{
-pages.forEach(function (page) {       if (page.id === id) {         response.writeHead(200, {'Content-Type': 'text/html'});         response.end(typeof page.output === 'function'         ? page.output() : page.output);       }     });   }   if (!response.finished) {     response.writeHead(404);     response.end('Page Not Found');   } }).listen(8080);
-
+var fs = require('fs'); http.createServer(function (request, response) {   var lookup = path.basename(decodeURI(request.url)) ||       'index.html';   var f = 'content/' + lookup;   fs.exists(f, function (exists) {     console.log(exists ? lookup + " is there"     : lookup + " doesn't exist");   }); }).listen(8080);
 
