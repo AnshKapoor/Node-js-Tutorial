7a4bab935cad64aa4cf8974a5ebe50f69d230780
AnshKapoor <ansh605@gmail.com>
2019-05-26 01:05:20 +0530

    Using child-process module

--- content/event.js
+++ content/event.js
@@ -1,6 +1,6 @@
 const events = require('events');
 let emitter = new events.EventEmitter();
 emitter.on('newEvent',(message)=>{
-console.log('message:${message}');
+console.log(`message:${message}`);
 });
 emitter.emit('newEvent','Hello guys');
