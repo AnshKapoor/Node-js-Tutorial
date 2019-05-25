7b2e79c4e21ea282cad5ac07364b2dec8c55060c
AnshKapoor <ansh605@gmail.com>
2019-05-26 00:39:04 +0530

    Created a custom event using event emitter

--- content/script.js
+++ content/script.js
@@ -2,7 +2,7 @@ var readline = require('readline');
 var util = require('util');
 var RL = readline.createInterface(process.stdin,process.stdout);
 RL.question('What is your name',(name)=>{ // New way to create a function in ES6
-    RL.setPrompt('${name} how old are you');
+    RL.setPrompt('${name}how old are you');
     RL.prompt();
     RL.on('line',(age)=>{
         if(age<18){
