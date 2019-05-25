e39803a1a26449018562e52ea017ef5679259b8e
AnshKapoor <ansh605@gmail.com>
2019-05-26 00:11:25 +0530

    Using Readline module in script.js

--- content/script.js
+++ content/script.js
@@ -1,1 +1,5 @@
-window.onload = function() { alert('Yay Node!'); };
+var readline = require('readline');
+var RL = readline.createInterface(process.stdin,process.stdout);
+RL.question('What is your name',(name)=>{ // New way to create a function in ES6
+    console.log(name);
+})
