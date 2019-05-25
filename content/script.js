032964c68db171191c5939d9e4f8f9fbcb828b31
AnshKapoor <ansh605@gmail.com>
2019-05-26 00:22:16 +0530

    Using util module

--- content/script.js
+++ content/script.js
@@ -1,5 +1,14 @@
 var readline = require('readline');
+var util = require('util');
 var RL = readline.createInterface(process.stdin,process.stdout);
 RL.question('What is your name',(name)=>{ // New way to create a function in ES6
-    console.log(name);
+    RL.setPrompt('${name} how old are you');
+    RL.prompt();
+    RL.on('line',(age)=>{
+        if(age<18){
+            util.log('${name.trim()} because you are ${age} years old,you cannor proceed')
+            RL.close();        
+        }
+
+    })
 })
